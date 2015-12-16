811e5505269e39008a2affe20a2bac3facdc62e9
CJ-Zheng <897839393@qq.com>
2015-12-16 23:51:39 +0800

    v1.0.0

--- js/jquery.messageScroll.js
+++ js/jquery.messageScroll.js
@@ -11,7 +11,7 @@
  * $(selector).messageScroll({
  *     delay:               //滚动间隔时间                    默认50ms
  * 	   mouseOverStop:       //鼠标放上去是否停止滚动           默认true
- * 	   direction:           //滚动方向  up or down            默认up     //TODO   这个功能还没做
+ * 	   direction:           //滚动方向  up or down            默认up
  * 	   messageArea:         //消息存放区域                    必填
  * })
  * 
@@ -25,8 +25,12 @@
  * 
  * 
  * 
- * 
- * 
+ *
+ *
+ * update                    v1.0.0  date 2015-12-16
+ * @author zhengchj
+ *
+ * 1.   新增向下滚动的功能
  */
 
 
@@ -41,11 +45,15 @@
 		if(settings.messageArea==null||settings.messageArea=="")    //v0.1.1
 			return this;
 		settings.$this=this;
-		$(settings.messageArea).css("margin",0);
+        //$(settings.messageArea).css("margin",0);
 		$(settings.messageArea).next().remove();
 		$(settings.$this).append($(settings.messageArea).clone(true));   //v0.1.1
 		$(settings.messageArea).next().attr("id","scroll_clone");
-		$(settings.$this).scrollTop(0).css("overflow","hidden");
+        if(ifUp(settings.direction)){
+            $(settings.$this).scrollTop(0).css("overflow","hidden");
+        }else{
+            $(settings.$this).scrollTop(2*$(settings.messageArea).outerHeight(true)).css("overflow","hidden");
+        }
 		function scrollUp(){
 			if($(settings.$this).scrollTop()>=$(settings.messageArea).outerHeight(true)){
 				$(settings.$this).scrollTop(0);
@@ -54,14 +62,26 @@
 				$(settings.$this).scrollTop(currentScrollTop+1);
 			}
 		}
-		var scrollUpInterval=setInterval(scrollUp,settings.delay);
+        function scrollDown(){  //v1.0.0
+            if($(settings.$this).scrollTop()<=$(settings.messageArea).outerHeight(true)-$(settings.$this).height()){
+                $(settings.$this).scrollTop(2*$(settings.messageArea).outerHeight(true));
+            }else{
+                var currentScrollTop=$(settings.$this).scrollTop();
+                $(settings.$this).scrollTop(currentScrollTop-1);
+            }
+        }
+        function ifUp(direction){   //v1.0.0
+            return direction=="up";
+        }
+        var scrollFunction=ifUp(settings.direction)?scrollUp:scrollDown
+		var scrollInterval=setInterval(scrollFunction,settings.delay);
 		if(settings.mouseOverStop){
 			$(settings.$this).bind({
 				mouseover:function(){
-					clearInterval(scrollUpInterval);
+					clearInterval(scrollInterval);
 				},
 				mouseout:function(){
-					scrollUpInterval=setInterval(scrollUp,settings.delay);
+                    scrollInterval=setInterval(scrollFunction,settings.delay);
 				}
 			})
 		}
